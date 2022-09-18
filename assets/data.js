6faaf9b572353e2215d174df8ea003aec644914b
juliobarbagallo <julio.barbagallo@gmail.com>
2022-09-18 17:15:50 -0300

    Adds static images and links to ctalogs

--- assets/data.js
+++ assets/data.js
@@ -5,7 +5,8 @@ export const equipos = [
     descripcion:
       "La bomba de infusión a jeringa S7 posee un tamaño pequeño,  peso ligero y diseño modular y cuenta con pantalla táctil para facilitar y acelerar la operación. Fácil de transportar, apilada y puede formar un multi-canal de infusión.",
     caracteristicas: ["caracteristica 1", "caracteristica 2" ],
-    imagen: "/Bomba-de-infusión-a-jeringa-S7.png",
+    imagen: "/S7 SIDE.png",
+    catalogo: "https://drive.google.com/file/d/1AJvK2DQKoKGH8TkfPwEaduNbguBD5NdD/view?usp=sharing",
   },
   {
     id: "equipo2",
@@ -13,97 +14,187 @@ export const equipos = [
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
     caracteristicas: ["caracteristica 1", "caracteristica 2" ],
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    imagen: "/i7 SIDE (1).png",
+    catalogo: "https://drive.google.com/file/d/1XQNhYnym0UxceWjcWD18UtlsuUaI-V5E/view?usp=sharing",
   },
   {
     id: "equipo3",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Bomba de infusión a jeringa TCI-7",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/TCI-7.png",
+    catalogo: "https://drive.google.com/file/d/13BdO544d6fIo_aZysPLoZ-ZW6rn9JuMF/view?usp=sharing",
   },
   {
     id: "equipo4",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Bomba de infusión volumétrica Infu Z-1000",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/Infu.jpg",
+    catalogo: "https://drive.google.com/file/d/1imOmE2Q9QmHJNCphyOKRZNMGTVtvJdI0/view?usp=sharing",
   },
   {
     id: "equipo5",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "D-Heart",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/dheart.png",
+    catalogo: "https://drive.google.com/file/d/1K-xS7K20Lj8KmIFbtI1puRcUk__zM8tv/view?usp=sharing",
   },
   {
     id: "equipo6",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Desfibrilador externo automático IPAD",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
-  },
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/ipad.jpeg",
+    catalogo: "https://drive.google.com/file/d/1vWkfmlCmqp3_paAllxU-aorrwTC1TOLg/view?usp=sharing",
+  }, 
   {
     id: "equipo7",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Desfibrilador Lifegain",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/lifegain.jpeg",
+    catalogo: "https://drive.google.com/file/d/1wMndyCYbinxg5cT1y7B6Ty3gl4BUNkJe/view?usp=sharing",
   },
-  {
+ {
     id: "equipo8",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electrocardiógrafo ECG 90G",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/ecg90.jpg",
+    catalogo: "https://drive.google.com/file/d/1J33OcgZutGYNVXoHSaKT_f6ZHO0jhD4p/view?usp=sharing",
   },
-  {
+ {
     id: "equipo9",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electrocardiógrafo ECG 100G",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/ECG100.jpg",
+    catalogo: "https://drive.google.com/file/d/1p_XsZQA4wn2u7B5-1lXGnwkChUyQCEzx/view?usp=sharing",
   },
-  {
+ {
     id: "equipo10",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electrocardiógrafo ECG 300G",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/ECG300.jpg",
+    catalogo: "https://drive.google.com/file/d/1486NSvDaZWcaRV-8AcRuk-lyFLytPAGf/view?usp=sharing",
   },
-  {
+ {
     id: "equipo11",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electrocardiógrafo ECG 600G",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/ecg600.jpg",
+    catalogo: "https://drive.google.com/file/d/1t9y-XwiOIww1sSGJanimb78ne8s0czK6/view?usp=sharing",
   },
-  {
+ {
     id: "equipo12",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electroencefalógrafo KT88",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/kt88.jpg",
+    catalogo: "https://drive.google.com/file/d/1TRVmi0DkimzrvcjfNuqpljgg5lNlIf1g/view?usp=sharing",
   },
-  {
+ {
     id: "equipo13",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Electroencefalógrafo KT88 2400",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/kt882400.jpg",
+    catalogo: "https://drive.google.com/file/d/1jIsdirUkDpqcX52prnQC64_v8Tpw9Umx/view?usp=sharing",
   },
-  {
+ {
     id: "equipo14",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Espirómetro SP80B",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/sp80.png",
+    catalogo: "https://drive.google.com/file/d/1ix0JFKJGLpKbRjMmaLSZobKZPwwxalFc/view?usp=sharing",
   },
-  {
+ {
     id: "equipo15",
-    nombre: "Bomba de infusión volumétrica I7",
+    nombre: "Maquina de Anestesia Full-V6",
     descripcion:
       "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
-    imagen: "/Bombas-de-infusion-volumetrica-I7-500x500.jpg",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/fullv6.png",
+    catalogo: "https://drive.google.com/file/d/15Ty2oVU2bqdayS6e793eOsJNgU-Qw6m5/view?usp=sharing",
+  },
+{
+    id: "equipo16",
+    nombre: "Maquina de Anestesia Full-V7",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/fullv7.png",
+    catalogo: "https://drive.google.com/file/d/1i2jb4N3pn0WAQTuks7ri-OylC_b5gqml/view?usp=sharing",
+  },
+{
+    id: "equipo17",
+    nombre: "Maquina de Anestesia Full-V9",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/fullv9.jpg",
+    catalogo: "https://drive.google.com/file/d/1hst2YiX5B_t22ALWl17nl5ww8S1RJdrl/view?usp=sharing",
+  },
+{
+    id: "equipo18",
+    nombre: "Maquina de Anestesia Full-V6",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/fullv6.png",
+    catalogo: "https://drive.google.com/file/d/15Ty2oVU2bqdayS6e793eOsJNgU-Qw6m5/view?usp=sharing",
+  },
+{
+    id: "equipo19",
+    nombre: "Monitor multiparamétrico Touch-Q8",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/Q8.jpg",
+    catalogo: "https://drive.google.com/file/d/128PPckY54ld-QwK6RczsbEtLDqpLMsYZ/view?usp=sharing",
+  },
+{
+    id: "equipo20",
+    nombre: "Monitor multiparamétrico Touch-Q12",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/Q12.jpg",
+    catalogo: "https://drive.google.com/file/d/1_LeG1X52V_UHY6S0dxXeatRH4hXk2FAl/view?usp=sharing",
+  },
+{
+    id: "equipo21",
+    nombre: "Monitor multiparamétrico Touch-Q15",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/Q15.png",
+    catalogo: "https://drive.google.com/file/d/1PzgTJLcKafIIsZRUHd6zpno7YeqOld9Q/view?usp=sharing",
+  },
+{
+    id: "equipo22",
+    nombre: "Monitor multiparamétrico Touch-Q15a",
+    descripcion:
+      "La bomba de infusión volumétrica I7 posee un sistema de operación intuitivo, función de valoración, seguridad de nueve alarmas, detección de burbujas y modo de purga automática. Cuenta con una apariencia moderna con sistema táctil inteligente, nueve modos de infusión, memoria de última programación e historial de eventos y librería con los medicamentos más usados.",
+    caracteristicas: ["caracteristica 1", "caracteristica 2" ],
+    imagen: "/Q15a.JPG",
+    catalogo: "https://drive.google.com/file/d/1jFZs-1a0vBGr1HA5dLaFihbPdR2gJK-p/view?usp=sharing",
   },
 ];
